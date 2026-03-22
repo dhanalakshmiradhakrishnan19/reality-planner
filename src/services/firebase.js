e635e7ae485adf6f31f4404cd52dd3ec72189e80
Dhanalakshmi Radhakrishnan <dhanalakshmiradha19@gmail.com>
2026-03-22 19:34:44 +0530

    Fix security

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGCDxeMcttXZj1wXUKUW-T2Nrm4ZFuY4U",
-  authDomain: "reality-planner-b4a9c.firebaseapp.com",
-  projectId: "reality-planner-b4a9c",
-  storageBucket: "reality-planner-b4a9c.firebasestorage.app",
-  messagingSenderId: "869089937149",
-  appId: "1:869089937149:web:2b9fc89990a426ba232094"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
