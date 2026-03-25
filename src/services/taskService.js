51c6831701ce159d88b0d2c13dc4b8d7bbc75aa6
dhanalakshmiradhakrishnan19 <dhanalakshmiradha19@gmail.com>
2026-03-25 17:12:27 +0530

    Update taskService.js

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,19 +1,15 @@
 import { db } from "./firebase";
 import { collection, addDoc, doc, updateDoc, deleteDoc, query, where, onSnapshot } from "firebase/firestore";
-
 export const addTaskToDB = async (task, userId) => {
   await addDoc(collection(db, "tasks"), { ...task, userId });
 };
-
 export const updateTask = async (id, data) => {
   const taskRef = doc(db, "tasks", id);
   await updateDoc(taskRef, data);
 };
-
 export const deleteTask = async (id) => {
   await deleteDoc(doc(db, "tasks", id));
 };
-
 export const subscribeToTasks = (userId, callback) => {
   const q = query(collection(db, "tasks"), where("userId", "==", userId));
   return onSnapshot(q, (snapshot) => {
